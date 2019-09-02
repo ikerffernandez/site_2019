6f97eb30688094ab63fb832c7968675cb6bf4d26
Iker <41619735+ikerffernandez@users.noreply.github.com>
2019-09-02 15:51:22 +0200

    Add files via upload
    New fade in and fadeout effect for work experience using jQuery

---
+++ js/main_js.js
@@ -0,0 +1,56 @@
+﻿//Sage
+
+
+$('.sage').on('mouseover', function () {
+    $('.experience-1-year-position').addClass('make-visible')
+});
+
+$('.sage').on('mouseout', function () {
+    $('.experience-1-year-position').removeClass('make-visible')
+});
+
+
+
+//Typeform
+
+$('.typeform').on('mouseover', function () {
+    $('.experience-2-year-position').addClass('make-visible')
+});
+
+$('.typeform').on('mouseout', function () {
+    $('.experience-2-year-position').removeClass('make-visible')
+});
+
+
+
+//Freelance
+
+$('.freelance').on('mouseover', function () {
+    $('.experience-3-year-position').addClass('make-visible')
+});
+
+$('.freelance').on('mouseout', function () {
+    $('.experience-3-year-position').removeClass('make-visible')
+});
+
+
+//Dommo
+
+$('.dommo').on('mouseover', function () {
+    $('.experience-4-year-position').addClass('make-visible')
+});
+
+$('.dommo').on('mouseout', function () {
+    $('.experience-4-year-position').removeClass('make-visible')
+});
+
+
+//Lirici Greci
+
+$('.liricigreci').on('mouseover', function () {
+    $('.experience-5-year-position').addClass('make-visible')
+});
+
+$('.liricigreci').on('mouseout', function () {
+    $('.experience-5-year-position').removeClass('make-visible')
+});
