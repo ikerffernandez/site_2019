c6cdd32feaab0bc26e1b75c9c5e2dddc6ae730b0
Iker <41619735+ikerffernandez@users.noreply.github.com>
2019-11-26 23:49:29 +0100

    new images

--- js/main_js.js
+++ js/main_js.js
@@ -2,11 +2,13 @@
 var image = document.getElementsByClassName('thumbnail-1');
 new simpleParallax(image, {
     orientation: 'up',
+    scale: 1.2,
 });
 
 var image = document.getElementsByClassName('thumbnail-2');
 new simpleParallax(image, {
     orientation: 'down',
+    scale: 1.2,
 });
 
 //Scroll progress
